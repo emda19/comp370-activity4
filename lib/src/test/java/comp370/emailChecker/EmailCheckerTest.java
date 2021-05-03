cb20e6b2c423e4524af6ea19949dee06dc11b838
Emily Daly <edaly3@luc.edu>
2021-05-03 16:07:59 -0500

    build.gradle updated

---
+++ lib/src/test/java/comp370/emailChecker/EmailCheckerTest.java
@@ -0,0 +1,24 @@
+package comp370.emailChecker;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailCheckerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
